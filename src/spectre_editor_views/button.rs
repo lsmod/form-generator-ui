c975485c1ccf7d4986f933b735c9aa160966bc5f
lsmod <1526305+lsmod@users.noreply.github.com>
2020-06-16 10:49:20 +0200

    adds TODO

--- src/spectre_editor_views/button.rs
+++ src/spectre_editor_views/button.rs
@@ -1,8 +1,13 @@
 use yew::{html, Component, Callback, ComponentLink,Properties, Html, ShouldRender};
 
+// TODO adds icons props add primary, secondary (outline)
+
 #[derive(Properties, Clone)]
 pub struct Props {
     pub text: String,
+    // TODO make it optionnal by having an option
+    // @see https://dev.to/deciduously/lets-build-a-rust-frontend-with-yew---part-2-1ech
+    // @see https://github.com/deciduously/hunt-the-wumpus/blob/66938953772f75051658a222d2643ed881db694c/part2/src/components/controls.rs
     #[props(required)]
     pub onclick: Callback<()>,
 }
